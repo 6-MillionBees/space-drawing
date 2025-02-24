1999722b43d3d063a7465ce0fa9b53aece6f421f
6MillionBees <BoettcherAr321@northwested.org>
2025-02-24 09:51:43 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+# Arden Boettcher
+# 2/11/25
+# Template Config
+
+# Screen size constants
+WIDTH = 500
+HEIGHT = 500
+
+# Framerate
+FPS = 60
+
+# Color Constants
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+RED = (255, 0, 0)
+BLUE = (0, 255, 0)
+GREEN = (0, 0, 255)
